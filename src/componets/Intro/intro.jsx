5ba0ffb8dbeaf17862406e1365fd33f70bca8213
arun-kunwar <arunkunwar069@gmail.com>
2023-09-07 20:04:58 +0545

    second commit

--- src/componets/Intro/intro.jsx
+++ src/componets/Intro/intro.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
-import'./intro.css'
+import './intro.css'
 
 function body() {
   return (
-      <>
-         <section className='intro'>
-              <h1> We Provide Expert Financial Advice</h1>
-              <p>we provides independent advice  based on establised <br />reasearch methods and our experts have in-depth sector knowledge</p>
+    <>
+      <section className='flex'  > <div className='intro'> 
+        <h1> We Provide Expert Financial Advice</h1>
+        <p>we provides independent advice  based on establised <br />reasearch methods and
+          our experts have in-depth sector knowledge</p></div>
+       
+        <div className='box'>
+
+       
          
-              </section>
+          
+
       
-      </>
+        </div>
+        </section>
+      
+
+      
+
+    </>
   )
 }
 
--- src/componets/NavBar/Nav.jsx
+++ src/componets/NavBar/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './nav.css';
-import logo from './images/logo.jpg';
+import logo from './images/logo.png';
 
 function Nav() {
     return (
